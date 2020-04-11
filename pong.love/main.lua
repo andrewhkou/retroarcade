8f3c5562f6794cec840acae26c93deea7893f4d9
Andrew Kou <andrewhkou@gmail.com>
2020-04-11 02:38:04 -0700

    added velocities

--- pong.love/main.lua
+++ pong.love/main.lua
@@ -12,35 +12,78 @@ function love.load()
     paddleWidth = 20
     paddleHeight = 70
 
-    paddlex1 = 50
-    paddley1 = (screenDimY / 2) -  (paddleHeight / 2)
+    paddle1 = {
+        x = 50,
+        y = (screenDimY / 2) -  (paddleHeight / 2);
+        vel= 0;
+    }
 
-    paddlex2 = screenDimX - 50 - paddleWidth
-    paddley2 = (screenDimY / 2) -  (paddleHeight / 2)
+    paddle2 = {
+        x = screenDimX - 50 - paddleWidth,
+        y = (screenDimY / 2) -  (paddleHeight / 2), 
+        vel = 0;
+    }
+
+
+    ball = {
+        ballX = 0,
+        ballY = 0,
+        velX = 0,
+        velY = 0
+    }
 
     love.window.setTitle("Pong");
     love.window.setMode(screenDimX, screenDimY)
+
+    function paddle1Collision(ball, paddle1)
+        return true 
+    end
+
+    function paddle2Collision(ball, paddle2)
+        return true
+    end
+
+    function wallTopCollision(ball)
+        return true
+    end
+
+    function wallBottomCollision(ball)
+        return true
+    end
 end
 
 function love.update(dt) 
     timeElapsed = timeElapsed + dt;
     totalTimeElapsed = totalTimeElapsed + 1;
-    if love.keyboard.isDown('w') and paddley1 > 0 then
-        paddley1 = paddley1 - paddleSpeed
+
+    if not love.keyboard.isDown('w') or not love.keyboard.isDown('s') then
+        paddle1.vel = 0;
     end
-    if love.keyboard.isDown('s') and (paddley1 + paddleHeight) < screenDimY then
-        paddley1 = paddley1 + paddleSpeed 
+
+    if not love.keyboard.isDown('up') or not love.keyboard.isDown('down') then
+        paddle2.vel = 0;
     end
-    if love.keyboard.isDown('up') and paddley2 > 0 then
-        paddley2 = paddley2 - paddleSpeed
+
+    if love.keyboard.isDown('w') and paddle1.y > 0 then
+        paddle1.y = paddle1.y - paddleSpeed
+        paddle1.vel = 5
     end
-    if love.keyboard.isDown('down') and (paddley2 + paddleHeight) < screenDimY then
-        paddley2 = paddley2 + paddleSpeed
+    if love.keyboard.isDown('s') and (paddle1.y + paddleHeight) < screenDimY then
+        paddle1.y = paddle1.y + paddleSpeed 
+        paddle1.vel = -5
+    end
+    if love.keyboard.isDown('up') and paddle2.y > 0 then
+        paddle2.y = paddle2.y - paddleSpeed
+        paddle2.vel = 5
+    end
+    if love.keyboard.isDown('down') and (paddle2.y + paddleHeight) < screenDimY then
+        paddle2.y = paddle2.y + paddleSpeed
+        paddle2.vel = -5
     end
 end
 
 function love.draw() 
     love.graphics.line(screenDimX/2, screenDimY, screenDimX/2, 0)
-    love.graphics.rectangle('fill', paddlex1, paddley1, paddleWidth, paddleHeight)
-    love.graphics.rectangle('fill', paddlex2, paddley2, paddleWidth, paddleHeight)
+    love.graphics.rectangle('fill', paddle1.x, paddle1.y, paddleWidth, paddleHeight)
+    love.graphics.rectangle('fill', paddle2.x, paddle2.y, paddleWidth, paddleHeight)
 end
