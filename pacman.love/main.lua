9f0d6f7de561cb235e38dfb25abea7cb201801be
Oscar Hu <oscarhu@berkeley.edu>
2020-04-14 22:44:28 -0700

    pacman add

---
+++ pacman.love/main.lua
@@ -0,0 +1,16 @@
+screenDimX = 1280;
+screenDimY = 720;
+
+function love.load()
+    love.window.setTitle("Pacman");
+    love.window.setMode(screenDimX, screenDimY)
+    love.graphics.setNewFont(40)
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw()
+
+end
